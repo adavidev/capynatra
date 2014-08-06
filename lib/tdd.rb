fff1ec700b1859e723090d383113a6066bbfe19d
Alan Davis <alan.davis@sage.com>
2014-08-06 01:38:38 -0400

    Added cart exercise

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,5 +1,25 @@
-require 'sinatra'
+class Cart
+  extend Forwardable
 
-get '/main' do
-  "hello world"
+  def initialize
+    @cart = []
+  end
+
+  def_delegator :@cart, :push, :add
+  def_delegator :@cart, :delete, :remove
+
+  def_delegators :@cart, :size
+
+  def total_cost
+    @cart.map(&:cost).inject(:+)
+  end
+end
+
+class Item
+  attr_accessor :cost
+
+  def initialize(cost)
+    @cost = cost
+  end
+
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,8 +1,38 @@
 require "spec_helper"
 
-describe "the signin process", :type => :feature do
-  it "signs me in" do
-    visit '/main'
-    expect(page).to have_content 'hello world'
+describe Cart do
+  let(:tdd_obj) { Cart.new }
+  subject { tdd_obj }
+
+
+  describe :add do
+    before :each do
+      subject.add("item")
+    end
+
+    it { expect(subject.size).to eql 1 }
+
   end
+
+  describe :remove do
+    before :each do
+      subject.add("item")
+      subject.remove("item")
+    end
+
+    it { expect(subject.size).to eql 0 }
+  end
+
+  describe :total_cost do
+
+    before :each do
+      subject.add(Item.new(23))
+      subject.add(Item.new(17))
+      subject.add(Item.new(17))
+    end
+
+    it { expect(subject.total_cost).to eql 57 }
+  end
+
+
 end
